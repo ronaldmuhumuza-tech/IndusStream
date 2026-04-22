671442059d60f6f3c3e18793312c8377eff0ead4
Ronald Muhumuza <ronwilmcd@gmail.com>
2026-04-22 22:28:22 +0100

    Add modified payload section in telemetry client

--- src/edge-simulator/telemetry_client.py
+++ src/edge-simulator/telemetry_client.py
@@ -3,13 +3,8 @@ import requests
 
 API_URL = "https://p6pg5m8627.execute-api.eu-west-2.amazonaws.com/telemetry"
 
-payload = {
-    "timestamp": "2026-04-23T21:00:00Z",
-    "device_id": "electrolyser-edge-01",
-    "temperature_c": 73.1,
-    "pressure_bar": 18.9,
-    "status": "normal"
-}
+with open("sample_payload.json") as f:
+    payload = json.load(f)
 
 response = requests.post(API_URL, json=payload, timeout=10)
 
